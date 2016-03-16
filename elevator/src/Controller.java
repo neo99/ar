a1164061ac4d6c88acc6a5b8a7a4121caaae193b
Neo Wang <neo.wang@powerreviews.com>
2016-03-15 21:53:38 -0700

    53 MINS

--- elevator/src/Controller.java
+++ elevator/src/Controller.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Neo Wang
  * @since 3/15/16
  */
 public class Controller {
-    public void go(int from, int to){
+    private List<Elevator> elevatorList = new ArrayList<Elevator>();
+
+    public void register(Elevator elevator){
+        elevatorList.add(elevator);
+    }
+
+    public void call(int from){
+
+    }
+
+    public void go(int to){
 
     }
 }
--- elevator/src/Elevator.java
+++ elevator/src/Elevator.java
@@ -5,4 +5,13 @@
 public class Elevator {
     private int trips = 0;
     private int top = 1;
+    private int currentFloor = 0;
+
+    public void call(int from){
+
+    }
+
+    public void go(int to){
+
+    }
 }
